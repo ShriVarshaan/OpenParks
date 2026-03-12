8e68ca5326491405da723c75718c432c6454603d
ShriVarshaan <varshaanvijay@gmail.com>
2026-03-12 22:05:19 +0000

    user validation middleware

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,12 +1,13 @@
 import express from "express"
 import {login, signup} from "../controllers/authController.js"
+import { validateUser, validateUserLogin } from "../middleware/user.js"
 
 const router = express.Router()
 
 router.route("/login")
-    .post(login)
+    .post(validateUserLogin, login)
 
 router.route("/signup")
-    .post(signup)
+    .post(validateUser, signup)
 
 export default router

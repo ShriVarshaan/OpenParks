d13c03e439e575cddada04df44083ff3eda27596
Yingcheng Yu <yxy527@student.bham.ac.uk>
2026-03-12 21:21:38 +0000

    Edit parkRoutes.js

--- backend/src/routes/parkRoutes.js
+++ backend/src/routes/parkRoutes.js
@@ -1,9 +1,20 @@
-import express from "express"
-import { getAllParks } from "../controllers/parkController.js"
+import express from "express";
+import {
+    getAllParks,
+    getParkById,
+    createPark,
+    updatePark,
+    deletePark,
+    getNearbyParks
+} from "../controllers/parkController.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.route("/")
-    .get(getAllParks)
+router.route("/").get(getAllParks);
+router.get("/:id", getParkById);
+router.post("/", createPark);
+router.put("/:id", updatePark);
+router.delete("/:id", deletePark);
+router.get("/nearby", getNearbyParks);
 
-export default router
+export default router;

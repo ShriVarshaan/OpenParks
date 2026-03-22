e8236d0e61604f80eaf78eab0938cf64f67cbe14
Hongyi Qin <hxq434@student.bham.ac.uk>
2026-03-22 12:20:26 +0000

    Edit vitest.config.js

--- frontend/vitest.config.js
+++ frontend/vitest.config.js
@@ -2,19 +2,19 @@ import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [react()],
-    test: {
-        environment: 'jsdom',
-        globals: true,
-        setupFiles: ['./test/setup.js'],
-        include: ['test/**/*.test.{js,jsx}'],
-        coverage: {
-            provider: 'v8',
-            reporter: ['text', 'html'],
-            exclude: [
-                'node_modules/',
-                'test/',
-            ],
-        },
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./test/setup.js'],
+    include: ['test/**/*.test.{js,jsx}'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      exclude: [
+        'node_modules/',
+        'test/',
+      ],
     },
+  },
 })
